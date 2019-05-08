eb685a8c19b1c7c44d55f4295b603c3e6e4947b5
eudelvigio <32935912+eudelvigio@users.noreply.github.com>
2019-05-08 12:43:58 +0200

    Update ejercicio6.js
    Added spread

--- ejercicio6.js
+++ ejercicio6.js
@@ -5,13 +5,15 @@
 // o bien el defaultvalue.
 
 // Como approaches que he seguido para la copia profunda, los siguientes comentarios:
-// JSON.parse(JSON.stringify(obj)) tiene problemas con estructuras circulares en el json
+// JSON.parse(JSON.stringify(obj)) tiene problemas con estructuras circulares en el json, y no es capaz de copiar fechas ni funciones
 // Object.assign({}, obj) tiene problemas con subobjetos
+// {...obj} tiene problemas con funciones y subobjetos
 // El método implementado deepCopy copia todos los subobjetos correctamente, necesita soporte ES6, y lo he encontrado en https://stackoverflow.com/questions/40291987/javascript-deep-clone-object-with-circular-references
 
 function accesor(obj, def, path) {
 	// var recObj = JSON.parse(JSON.stringify(obj)); Método con problemas para JSON circulares
 	// var recObj = Object.assign({}, obj); Método con problemas para subobjetos
+	// var recObj = {...obj}; Método con problemas para funciones
 	var recObj = deepClone(obj);
 	function safeAccesor(path) {
 		var splittedPath = path.split('.');
@@ -32,6 +34,7 @@ function accesor(obj, def, path) {
 	}
 }
 
+// Obtenido de https://stackoverflow.com/questions/40291987/javascript-deep-clone-object-with-circular-references
 function deepClone(obj, hash = new WeakMap()) {
     // Do not try to clone primitives or functions
     if (Object(obj) !== obj || obj instanceof Function) return obj;
